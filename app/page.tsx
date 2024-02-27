655e5e72bb531d6ebc1845d73ebc816ab0c9b535
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-02-26 21:30:18 -0800

    Add bitmap pfp, tagline, now, and connect

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,30 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home () {
   return (
-    <h1>Hello</h1>
+    <>
+      <Image
+        unoptimized
+        src="/assets/pfp-bitmap.png"
+        alt="Vercel Logo"
+        width={200}
+        height={200}
+      />
+      <h1>Edward Shturman</h1>
+      <p>Web developer, interface designer, and community builder based in San Francisco</p>
+      <h2>Now</h2>
+      <p>Focused on running <Link href="https://github.com/compsigh">compsigh</Link>, my CS club</p>
+      <h2>Connect</h2>
+      <p>
+        <Link href="https://twitter.com/edwardshturman">Twitter</Link>
+      </p>
+      <p>
+        <Link href="https://github.com/edwardshturman">GitHub</Link>
+      </p>
+      <p>
+        <Link href="https://read.cv/edwardshturman">read.cv</Link>
+      </p>
+    </>
   )
 }
