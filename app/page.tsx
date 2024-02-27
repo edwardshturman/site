16fbd3f855696da333ef3d718b32a90f485fd5dd
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-02-26 21:55:13 -0800

    Add WIP note

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,8 @@ export default function Home () {
       />
       <h1>Edward Shturman</h1>
       <p>Web developer, interface designer, and community builder based in San Francisco</p>
+      <br />
+      <p>Note: I&apos;m in the process of moving content from <Link href="https://mmm.page/edday/edward">my old site</Link>, you can visit it if you&apos;d like to check out my work in the meantime.</p>
       <h2>Now</h2>
       <p>Focused on running <Link href="https://github.com/compsigh">compsigh</Link>, my CS club</p>
       <h2>Connect</h2>
