5010ab8a461c4562458fca4c33f1a6dbe2942ad9
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-02-26 18:02:35 -0800

    Run create-next-app

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    typedRoutes: true
+  }
+}
+
+export default nextConfig
