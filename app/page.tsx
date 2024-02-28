925f0aac7d46e68990e998637a102f31eb5490ae
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-02-27 19:43:55 -0800

    Add links to site pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,19 @@
-import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Home () {
   return (
     <>
-      <Image
-        unoptimized
-        src="/assets/pfp-bitmap-transparent.png"
-        alt="A bitmapped image of Edward Shturman"
-        width={200}
-        height={200}
-      />
       <h1>Edward Shturman</h1>
       <p>Web developer, interface designer, and community builder based in San Francisco</p>
       <br />
+      <nav>
+        <p>/ <Link href='/todos'>Todos</Link></p>
+        <p>/ <Link href='/notes'>Notes</Link></p>
+      </nav>
+      <br />
       <p>Note: I&apos;m in the process of moving content from <Link href="https://mmm.page/edday/edward">my old site</Link>, you can visit it if you&apos;d like to check out my work in the meantime.</p>
       <h2>Now</h2>
-      <p>Focused on running <Link href="https://github.com/compsigh">compsigh</Link>, my CS club</p>
+      <p>Focused on running <Link href="https://github.com/compsigh">compsigh</Link>, my CS club at the University of San Francisco</p>
       <h2>Connect</h2>
       <p>
         <Link href="https://twitter.com/edwardshturman">Twitter</Link>
