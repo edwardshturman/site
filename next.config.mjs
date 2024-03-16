62ede9bce7063f252ed5b6db5b50fb18665c51d1
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-03-15 23:57:02 -0700

    Remove `@next/mdx`

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,4 @@
 // @ts-check
-import createMDX from '@next/mdx'
-import rehypePrettyCode from 'rehype-pretty-code'
-
-const vercelTheme = await fetch(
-  'https://raw.githubusercontent.com/triyanox/vercel-theme/b93488a9a13216371ccdf015f3dc9f736dacb3e2/themes/Vercel%20Theme-color-theme.json'
-)
-const vercelThemeJson = await vercelTheme.json()
-
-/** @type {import('rehype-pretty-code').Options} */
-const options = {
-  theme: vercelThemeJson,
-  defaultLang: 'plaintext'
-}
-
-const withMDX = createMDX({
-  options: {
-    rehypePlugins: [[rehypePrettyCode, options]]
-  }
-})
 
 /**
  * @type {import('next').NextConfig}
@@ -26,8 +7,8 @@ const nextConfig = {
   experimental: {
     typedRoutes: true
   },
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
   reactStrictMode: true
 }
 
-export default withMDX(nextConfig)
+export default nextConfig
