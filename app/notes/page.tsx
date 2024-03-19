fb7a539cf38c1edc07536fa69225b942d03f59a4
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-03-18 21:18:23 -0700

    Update and publish "It's easy"

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import Link from 'next/link'
-import { Comment } from '@/app/components/Comment'
 
 export const metadata: Metadata = {
   title: 'Notes',
@@ -16,14 +15,11 @@ export default function Notes() {
       <h1>Notes</h1>
       <ul>
         <li>
+          <Link href="/notes/easy">&quot;It&apos;s easy&quot;</Link>
+        </li>
+        <li>
           <Link href="/notes/mandarin">Mandarin learning log</Link>
         </li>
-      </ul>
-      <br />
-      <Comment type="block">Writing</Comment>
-      <ul>
-        <li>Setting up MDX on Next.js 14</li>
-        <li>VS Code: an Artist&apos;s Canvas</li>
       </ul>
     </>
   )
