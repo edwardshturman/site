0ebb589778ef7968251fca4e5c2bbedb7ba5b2c0
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-04-14 21:16:04 -0700

    Add Latest section on homepage promoting cascade

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import Link from 'next/link'
+import Image from 'next/image'
+
 import { Spacer } from '@/components/Spacer'
+import { GalleryCard } from '@/components/GalleryCard'
 
 export default function Home() {
   return (
@@ -25,6 +28,25 @@ export default function Home() {
           </li>
         </ul>
       </nav>
+      <h2>Latest</h2>
+      <GalleryCard
+        title="compsigh cascade"
+        description={
+          <>
+            <p>A platform I design-engineered to host a coding riddle competition for {<Link href={"/projects/compsigh"}>compsigh</Link>}, my computer science club. Fit with auth via NextAuth, event flags via Vercel Edge Config, teams, invites, and leaderboard via Vercel Postgres, and input validation.</p>
+            <Spacer size={10} />
+            <p><Link href="https://github.com/compsigh/cascade">View source</Link></p>
+          </>
+        }
+      >
+        <Image
+          unoptimized
+          src="/assets/cascade-ui.gif"
+          alt="A gif of the compsigh cascade UI"
+          width={1920/4}
+          height={1080/4}
+        />
+      </GalleryCard>
       <h2>Now</h2>
       <p>
         Focused on running <Link href="/projects/compsigh">compsigh</Link>, my CS club at the University of San Francisco
