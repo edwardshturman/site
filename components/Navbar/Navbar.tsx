c7e7f58e438bc294b7c73f690f2b01d3bc4169c4
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2026-02-08 15:07:48 -0800

    Hide some links for now

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,37 +1,40 @@
-'use client'
+"use client"
 
-import Link from 'next/link'
-import { useEffect, useState } from 'react'
+import Link from "next/link"
+import { useEffect, useState } from "react"
 
-import styles from './Navbar.module.css'
+import styles from "./Navbar.module.css"
 
 export function Navbar() {
   const [isWindows, setIsWindows] = useState(false)
   useEffect(() => {
-    setIsWindows(navigator.userAgent.includes('Win'))
+    setIsWindows(navigator.userAgent.includes("Win"))
   }, [])
 
   return (
     <>
-      <nav id={styles.navbar} className={`${isWindows ? styles.windows : ''}`}>
+      <nav id={styles.navbar} className={`${isWindows ? styles.windows : ""}`}>
         <ul>
-          <li>
+          {/*<li>
             <Link href={"/notes"}>Notes</Link>
-          </li>
-          <li>
+          </li>*/}
+          {/*<li>
             <Link href={"/people"}>People</Link>
-          </li>
-          <li>
+          </li>*/}
+          {/*<li>
             <Link href={"/toolbox"}>Toolbox</Link>
-          </li>
+          </li>*/}
           <li>
             <Link href={"/projects"}>Projects</Link>
           </li>
           <li>
             <Link href={"/workshop"}>Workshop</Link>
           </li>
-          <li>
+          {/*<li>
             <Link href={"/artifacts"}>Artifacts</Link>
+          </li>*/}
+          <li>
+            <Link href="https://twitter.com/edwardshturman">Twitter</Link>
           </li>
         </ul>
       </nav>
