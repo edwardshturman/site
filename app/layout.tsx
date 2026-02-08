834d9a5cf384bc24c11503812b22d15fbd57a959
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2026-02-08 15:18:57 -0800

    Update page centering strategy to flexbox

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,75 +1,75 @@
-import type { Metadata } from 'next'
-import localFont from 'next/font/local'
-import { Analytics } from '@vercel/analytics/react'
+import type { Metadata } from "next"
+import localFont from "next/font/local"
+import { Analytics } from "@vercel/analytics/react"
 
-import { Spacer } from '@/components/Spacer'
-import { BigAvatar } from '@/components/BigAvatar'
-import { Breadcrumbs } from '@/components/Breadcrumbs'
+import { Spacer } from "@/components/Spacer"
+import { Breadcrumbs } from "@/components/Breadcrumbs"
 
-import './globals.css'
+import "./globals.css"
 
 const iAWriterQuattro = localFont({
   src: [
     {
-      path: '../public/fonts/iAWriterQuattroS-Regular.woff2',
-      weight: '400',
-      style: 'normal'
+      path: "../public/fonts/iAWriterQuattroS-Regular.woff2",
+      weight: "400",
+      style: "normal"
     },
     {
-      path: '../public/fonts/iAWriterQuattroS-Italic.woff2',
-      weight: '400',
-      style: 'italic'
+      path: "../public/fonts/iAWriterQuattroS-Italic.woff2",
+      weight: "400",
+      style: "italic"
     },
     {
-      path: '../public/fonts/iAWriterQuattroS-Bold.woff2',
-      weight: '600',
-      style: 'normal'
+      path: "../public/fonts/iAWriterQuattroS-Bold.woff2",
+      weight: "600",
+      style: "normal"
     },
     {
-      path: '../public/fonts/iAWriterQuattroS-BoldItalic.woff2',
-      weight: '600',
-      style: 'italic'
+      path: "../public/fonts/iAWriterQuattroS-BoldItalic.woff2",
+      weight: "600",
+      style: "italic"
     }
   ],
-  variable: '--font-ia-writer-quattro'
+  variable: "--font-ia-writer-quattro"
 })
 
 const iAWriterMono = localFont({
   src: [
     {
-      path: '../public/fonts/iAWriterMonoS-Regular.woff2',
-      weight: '400',
-      style: 'normal'
+      path: "../public/fonts/iAWriterMonoS-Regular.woff2",
+      weight: "400",
+      style: "normal"
     },
     {
-      path: '../public/fonts/iAWriterMonoS-Italic.woff2',
-      weight: '400',
-      style: 'italic'
+      path: "../public/fonts/iAWriterMonoS-Italic.woff2",
+      weight: "400",
+      style: "italic"
     },
     {
-      path: '../public/fonts/iAWriterMonoS-Bold.woff2',
-      weight: '600',
-      style: 'normal'
+      path: "../public/fonts/iAWriterMonoS-Bold.woff2",
+      weight: "600",
+      style: "normal"
     },
     {
-      path: '../public/fonts/iAWriterMonoS-BoldItalic.woff2',
-      weight: '600',
-      style: 'italic'
+      path: "../public/fonts/iAWriterMonoS-BoldItalic.woff2",
+      weight: "600",
+      style: "italic"
     }
   ],
-  variable: '--font-ia-writer-mono'
+  variable: "--font-ia-writer-mono"
 })
 
 let metadataBase: URL
-if (process.env.VERCEL_URL) metadataBase = new URL('https://edwardshturman.com')
+if (process.env.VERCEL_URL) metadataBase = new URL("https://edwardshturman.com")
 else metadataBase = new URL(`http://localhost:${process.env.PORT || 3000}`)
 
 export const metadata: Metadata = {
   metadataBase,
-  title: 'Edward Shturman',
-  description: 'Web developer, interface designer, and community builder based in San Francisco',
+  title: "Edward Shturman",
+  description:
+    "Web developer, interface designer, and community builder based in San Francisco",
   openGraph: {
-    images: '/og/root.png'
+    images: "/og/root.png"
   }
 }
 
@@ -84,9 +84,18 @@ export default function RootLayout({
       className={`${iAWriterQuattro.variable} ${iAWriterMono.variable}`}
     >
       <body>
-        <BigAvatar />
         <Breadcrumbs />
-        <main>{children}</main>
+        <div
+          style={{
+            display: "flex",
+            width: "100vw",
+            height: "100vh",
+            alignItems: "center",
+            justifyContent: "center"
+          }}
+        >
+          <div style={{ maxWidth: 650 }}>{children}</div>
+        </div>
         <Spacer size="20vh" />
         <Analytics />
       </body>
