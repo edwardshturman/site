ae80495d93d021dd8d33b82c64e3302c9c98f3c8
Edward Shturman <60242511+edwardshturman@users.noreply.github.com>
2026-02-08 17:13:00 -0800

    Fix bio paragraph indentation on mobile (#5)

--- app/page.tsx
+++ app/page.tsx
@@ -23,20 +23,26 @@ export default function Home() {
             <h1 className={styles.name}>Edward Shturman</h1>
           </div>
         </div>
-        <p>
-          I&apos;m a web developer, interface designer, <br /> and community
-          builder based in San Francisco
-        </p>
-        <p>
-          I&apos;m an engineering intern on the Primitive Foundations team at
-          Notion, working on blocks, database views, and general polish across
-          the app
-        </p>
-        <p>
-          I study at the University of San Francisco, where I founded compsigh,
-          the social computer science club for meeting cool people &amp;&amp;
-          building cool things
-        </p>
+        <div className={styles.bio}>
+          <p>
+            I&apos;m a web developer, interface designer, <br /> and community
+            builder based in San Francisco
+          </p>
+          <p className={styles.mobile}>
+            I&apos;m a web developer, interface designer, and community builder
+            based in San Francisco
+          </p>
+          <p>
+            I&apos;m an engineering intern on the Primitive Foundations team at
+            Notion, working on blocks, database views, and general polish across
+            the app
+          </p>
+          <p>
+            I study at the University of San Francisco, where I founded
+            compsigh, the social computer science club for meeting cool people
+            &amp;&amp; building cool things
+          </p>
+        </div>
         <Spacer size={4} />
         <Navbar />
       </main>
