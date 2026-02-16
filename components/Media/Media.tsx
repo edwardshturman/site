4a6e0b6277c0dcf5a22ee239b306970aa64606a9
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2026-02-15 17:37:08 -0800

    Format `Media.tsx`

--- components/Media/Media.tsx
+++ components/Media/Media.tsx
@@ -1,12 +1,12 @@
-import Link from 'next/link'
-import { type Route } from 'next'
-import fs from 'node:fs/promises'
-import { getPlaiceholder } from 'plaiceholder'
-import Image, { type ImageProps } from 'next/image'
+import Link from "next/link"
+import { type Route } from "next"
+import fs from "node:fs/promises"
+import { getPlaiceholder } from "plaiceholder"
+import Image, { type ImageProps } from "next/image"
 
-import styles from './Media.module.css'
+import styles from "./Media.module.css"
 
-export interface MediaProps extends Omit<ImageProps, 'alt'> {
+export interface MediaProps extends Omit<ImageProps, "alt"> {
   title?: string
   description?: React.ReactElement | string
   link?: Route
@@ -14,50 +14,48 @@ export interface MediaProps extends Omit<ImageProps, 'alt'> {
   alt?: string
 }
 
-export async function Media(
-  { title, description, link, cta, alt, ...props }:
-  MediaProps
-) {
+export async function Media({
+  title,
+  description,
+  link,
+  cta,
+  alt,
+  ...props
+}: MediaProps) {
   async function processImage(src: string) {
     const file = await fs.readFile(src)
     const { base64, metadata } = await getPlaiceholder(file)
     return { base64, metadata }
   }
   let imageData
-  const video = props.src.toString().endsWith('.mp4')
+  const video = props.src.toString().endsWith(".mp4")
   if (!video) imageData = await processImage(`public${props.src}`)
-  const isGif = props.src.toString().endsWith('.gif')
+  const isGif = props.src.toString().endsWith(".gif")
 
   const ImageWrapper = (
     <>
-      { !video &&
+      {!video && (
         <Image
           unoptimized={isGif}
-          alt={alt || ''}
-          placeholder={`${isGif ? 'empty' : 'blur'}`}
+          alt={alt || ""}
+          placeholder={`${isGif ? "empty" : "blur"}`}
           blurDataURL={imageData?.base64}
-          sizes='(max-width: 700px) 100vw - 80px, 650px'
+          sizes="(max-width: 700px) 100vw - 80px, 650px"
           width={imageData?.metadata.width}
           height={imageData?.metadata.height}
           {...props}
         />
-      }
+      )}
     </>
   )
 
   const VideoWrapper = (
     <>
-      {
-        video &&
-          <video
-            autoPlay
-            loop
-            muted
-            playsInline
-          >
-            <source src={props.src as string} type="video/mp4" />
-          </video>
-      }
+      {video && (
+        <video autoPlay loop muted playsInline>
+          <source src={props.src as string} type="video/mp4" />
+        </video>
+      )}
     </>
   )
 
@@ -67,18 +65,19 @@ export async function Media(
 
   return (
     <>
-      <div
-        id={styles["media-container"]}
-        style={props.style}
-      >
+      <div id={styles["media-container"]} style={props.style}>
         <figure>
           <LinkWrapper>
             {ImageWrapper}
             {VideoWrapper}
             {title && <figcaption>{title}</figcaption>}
           </LinkWrapper>
-            {description && <div id={styles.description}>{description}</div>}
-            {cta && <p id={styles.cta}><LinkWrapper>{cta}</LinkWrapper></p>}
+          {description && <div id={styles.description}>{description}</div>}
+          {cta && (
+            <p id={styles.cta}>
+              <LinkWrapper>{cta}</LinkWrapper>
+            </p>
+          )}
         </figure>
       </div>
     </>
